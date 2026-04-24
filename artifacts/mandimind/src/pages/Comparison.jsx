0f22651fe280da2a45f236bdd96dc3a2e2d29d21
onb23 <omkarborade.11@gmail.com>
2026-04-24 23:22:47 +0530

    Update Comparison.jsx

--- artifacts/mandimind/src/pages/Comparison.jsx
+++ artifacts/mandimind/src/pages/Comparison.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useLanguage } from "../context/LanguageContext";
 import { fetchAvailableCrops } from "../utils/mandiAvailability";
-import { fetchCompare } from "../utils/api";
 import MandiCard from "../components/MandiCard";
 
 export default function Comparison() {
